ba1f48c08c689460f83075663e83c7ecb29eb39b
Fernando Karnagi <fernando.karnagi@ctc-g.com.sg>
2015-12-09 10:23:21 +0800

    Update YARNtest.sh
    - changed to 10M, no 10 GB.
    - added 'date' to display the date time when the script starts and ends

--- yarn/YARNtest.sh
+++ yarn/YARNtest.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
 # Verify this path corresponds to your installation
-HADOOP_PATH=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
 
+HADOOP_PATH=/apps/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
+date
 # Mapper containers
-for i in 2 4 8    
+for i in 2    
 do
    # Reducer containers
-   for j in 2 4 8 
+   for j in 2 
    do                 
       # Container memory
       for k in 512 1024 
@@ -18,18 +18,13 @@ do
          # JVM heap for reducers
          RED_MB=`echo "($k*0.8)/1" | bc` 
 
-         hadoop jar $HADOOP_PATH/hadoop-examples.jar teragen
-            -Dmapreduce.job.maps=$i -Dmapreduce.map.memory.mb=$k -Dmapreduce.map.java.opts.max.heap=$MAP_MB 100000000 
-            /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
+         /apps/cloudera/parcels/CDH/bin/hadoop jar $HADOOP_PATH/hadoop-examples.jar teragen -Dmapreduce.job.maps=$i -Dmapreduce.map.memory.mb=$k -Dmapreduce.map.java.opts.max.heap=$MAP_MB 100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-         hadoop jar $HADOOP_PATH/hadoop-examples.jar terasort
-            -Dmapreduce.job.maps=$i -Dmapreduce.job.reduces=$j -Dmapreduce.map.memory.mb=$k 
-            -Dmapreduce.map.java.opts.max.heap=$MAP_MB -Dmapreduce.reduce.memory.mb=$k 
-            -Dmapreduce.reduce.java.opts.max.heap=$RED_MB /results/ts-10GB-${i}-${j}-${k} 
-            1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
+          /apps/cloudera/parcels/CDH/bin/hadoop jar $HADOOP_PATH/hadoop-examples.jar terasort -Dmapreduce.job.maps=$i -Dmapreduce.job.reduces=$j -Dmapreduce.map.memory.mb=$k -Dmapreduce.map.java.opts.max.heap=$MAP_MB -Dmapreduce.reduce.memory.mb=$k -Dmapreduce.reduce.java.opts.max.heap=$RED_MB /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
-         hadoop fs -rmr -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-         hadoop fs -rmr -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+          /apps/cloudera/parcels/CDH/bin/hadoop fs -rmr -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
+          /apps/cloudera/parcels/CDH/bin/hadoop fs -rmr -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
       done
    done
 done
+date
