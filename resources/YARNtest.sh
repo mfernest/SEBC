c30eba5ff118f0eeda2043e207cbbf2b216762d7
LouisErnst <louis.k.ernst@accenture.com>
2016-03-24 12:41:52 -0700

    Update YARNtest.sh

--- resources/YARNtest.sh
+++ resources/YARNtest.sh
@@ -8,37 +8,45 @@ HADOOP_PATH=/opt/cloudera/parcels/CDH/bin
 echo Testing loop started on `date`
 
 # Mapper containers
-for i in 2    
+for i in 2 4
 do
    # Reducer containers
-   for j in 2 
-   do                 
+   for j in 2 4
+   do
       # Container memory
-      for k in 512 1024 
-      do                         
-         # Set mapper JVM heap 
-         MAP_MB=`echo "($k*0.8)/1" | bc` 
+      for k in 512 1024 2048
+      do
+         # Set mapper JVM heap
+         MAP_MB=`echo "($k*0.8)/1" | bc`
 
-         # Set reducer JVM heap 
-         RED_MB=`echo "($k*0.8)/1" | bc` 
+         # Set reducer JVM heap
+         RED_MB=`echo "($k*0.8)/1" | bc`
 
-         $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
+        $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
-                     100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
+                     100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err
 
-         $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
+         echo "Mapper containers: $i"
+         echo "Reducer containers: $j"
+         echo "Container memory: $k"
+         echo "Mapper JVM heap: $MAP_MB"
+         echo "Reducer JVM heap: $RED_MB"
+
+
+         time $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      -Dmapreduce.reduce.memory.mb=$k \
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
-                     /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
+                     /results/tg-10GB-${i}-${j}-${k} \
+                     /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err
 
-          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/tg-10GB-${i}-${j}-${k}            
+          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/ts-10GB-${i}-${j}-${k}            
       done
    done
 done
