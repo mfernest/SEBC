b4e1c0a0f7bf496d38324ab49255d231b1a5ce08
darikyang <darik@163.com>
2016-05-19 17:25:49 +0800

    Add files via upload

--- resources/tools/YARNtest.sh
+++ resources/tools/YARNtest.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Confirm the path values given below correspond to your installation
 
-HADOOP_MR=/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce
-HADOOP_PATH=/opt/cloudera/parcels/CDH/bin
+HADOOP_MR=/opt/cloudera/parcels/CDH-5.7.0-1.cdh5.7.0.p0.45/lib/hadoop-0.20-mapreduce
+HADOOP_PATH=/opt/cloudera/parcels/CDH-5.7.0-1.cdh5.7.0.p0.45/bin
 
 # Mark start of the loop
 echo Testing loop started on `date`
@@ -22,23 +22,24 @@ do
          # Set reducer JVM heap 
          RED_MB=`echo "($k*0.8)/1" | bc` 
 
-         $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
+        $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar teragen \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      100000 /results/tg-10GB-${i}-${j}-${k} 1>tera_${i}_${j}_${k}.out 2>tera_${i}_${j}_${k}.err                       
 
-         $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
+       $HADOOP_PATH/hadoop jar $HADOOP_MR/hadoop-examples.jar terasort \
                      -Dmapreduce.job.maps=$i \
                      -Dmapreduce.job.reduces=$j \
                      -Dmapreduce.map.memory.mb=$k \
                      -Dmapreduce.map.java.opts.max.heap=$MAP_MB \
                      -Dmapreduce.reduce.memory.mb=$k \
                      -Dmapreduce.reduce.java.opts.max.heap=$RED_MB \
+	             /results/tg-10GB-${i}-${j}-${k}  \
                      /results/ts-10GB-${i}-${j}-${k} 1>>tera_${i}_${j}_${k}.out 2>>tera_${i}_${j}_${k}.err                         
 
-          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
-          $HADOOP_PATH/hadoop fs -rmr -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
+        $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/tg-10GB-${i}-${j}-${k}                         
+        $HADOOP_PATH/hadoop fs -rm -r -skipTrash /results/ts-10GB-${i}-${j}-${k}                 
       done
    done
 done
